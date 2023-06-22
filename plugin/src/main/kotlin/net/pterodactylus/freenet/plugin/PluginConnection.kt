1a3bb92e6b77e4f15dd63097a73a73fefee3e629
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-06-22 21:01:49 +0200

    🚸 Declare exception that can be thrown

--- plugin/src/main/kotlin/net/pterodactylus/freenet/plugin/PluginConnection.kt
+++ plugin/src/main/kotlin/net/pterodactylus/freenet/plugin/PluginConnection.kt
@@ -1,7 +1,10 @@
 package net.pterodactylus.freenet.plugin
 
+import java.io.IOException
+
 interface PluginConnection {
 
+	@Throws(IOException::class)
 	fun sendMessage(parameters: Map<String, String>)
 
 }
