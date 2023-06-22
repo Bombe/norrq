1ffda01f19a7091af45b11fe3ccee26b5794c5be
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-06-23 00:24:36 +0200

    🎨 Implement connection caching
    A new connection will be requested when necessary, i.e. when sending fails.

--- plugin/src/main/kotlin/net/pterodactylus/freenet/plugin/jFCPlibPluginConnection.kt
+++ plugin/src/main/kotlin/net/pterodactylus/freenet/plugin/jFCPlibPluginConnection.kt
@@ -2,20 +2,31 @@ package net.pterodactylus.freenet.plugin
 
 import net.pterodactylus.fcp.FcpConnection
 import net.pterodactylus.fcp.FcpMessage
+import java.io.IOException
 import java.util.concurrent.atomic.AtomicLong
 
 @Suppress("ClassName")
-class jFCPlibPluginConnection(private val fcpConnection: FcpConnection, private val pluginName: String) : PluginConnection {
+class jFCPlibPluginConnection(private val connectionSupplier: () -> FcpConnection, private val pluginName: String) : PluginConnection {
 
 	override fun sendMessage(parameters: Map<String, String>) {
-		fcpConnection.sendMessage(buildPluginFcpMessage(parameters))
+		try {
+			getConnectionOrRequestNewConnection().sendMessage(buildPluginFcpMessage(parameters))
+		} catch (e: IOException) {
+			currentConnection = null
+			throw e
+		}
 	}
+
+	private fun getConnectionOrRequestNewConnection() =
+		currentConnection ?: connectionSupplier().also { currentConnection = it }
 
 	private fun buildPluginFcpMessage(parameters: Map<String, String>) = FcpMessage("FCPPluginMessage").apply {
 		put("PluginName", pluginName)
 		put("Identifier", "jFCPlibPluginConnection-$pluginName-${counter.getAndIncrement()}")
 		parameters.mapKeys { (key, _) -> "Parameter.$key" }.forEach(::put)
 	}
+
+	private var currentConnection: FcpConnection? = null
 
 }
 
--- plugin/src/test/kotlin/net/pterodactylus/freenet/plugin/jFCPlibPluginConnectionTest.kt
+++ plugin/src/test/kotlin/net/pterodactylus/freenet/plugin/jFCPlibPluginConnectionTest.kt
@@ -8,6 +8,8 @@ import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasEntry
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.io.IOException
 
 @Suppress("ClassName")
 class jFCPlibPluginConnectionTest {
@@ -27,12 +29,53 @@ class jFCPlibPluginConnectionTest {
 		assertThat(sentMessages.map { it.fields.get("Identifier") }.toSet(), hasSize(2))
 	}
 
+	@Test
+	fun `IO exception from fcp connection is thrown`() {
+		val fcpConnection = createThrowingFcpConnection()
+		val jFCPlibPluginConnection = createConnectionRequestCountingPluginConnection(fcpConnection)
+		assertThrows<IOException> { jFCPlibPluginConnection.sendMessage(mapOf("a" to "b", "aa" to "bb")) }
+	}
+
+	@Test
+	fun `sending the first message requests a new connection`() {
+		jFCPlibPluginConnection.sendMessage(mapOf("a" to "b", "aa" to "bb"))
+		assertThat(connectionRequests, equalTo(1))
+	}
+
+	@Test
+	fun `sending the second message does not request a new connection`() {
+		jFCPlibPluginConnection.sendMessage(mapOf("a" to "b", "aa" to "bb"))
+		jFCPlibPluginConnection.sendMessage(mapOf("a" to "c", "aa" to "cc"))
+		assertThat(connectionRequests, equalTo(1))
+	}
+
+	@Test
+	fun `sending a message after an IO exception on send requests a new connection`() {
+		val fcpConnection = createThrowingFcpConnection()
+		val jFCPlibPluginConnection = createConnectionRequestCountingPluginConnection(fcpConnection)
+		assertThrows<IOException> { jFCPlibPluginConnection.sendMessage(mapOf("a" to "b", "aa" to "bb")) }
+		assertThrows<IOException> { jFCPlibPluginConnection.sendMessage(mapOf("a" to "c", "aa" to "cc")) }
+		assertThat(connectionRequests, equalTo(2))
+	}
+
+	private fun createThrowingFcpConnection() = object : FcpConnection() {
+		override fun sendMessage(fcpMessage: FcpMessage) {
+			throw IOException("can’t send")
+		}
+	}
+
+	private fun createConnectionRequestCountingPluginConnection(fcpConnection: FcpConnection) = jFCPlibPluginConnection({
+		connectionRequests++
+		fcpConnection
+	}, "test.TargetPlugin")
+
+	private var connectionRequests = 0
 	private val sentMessages = mutableListOf<FcpMessage>()
 	private val fcpConnection = object : FcpConnection() {
 		override fun sendMessage(fcpMessage: FcpMessage) {
 			sentMessages += fcpMessage
 		}
 	}
-	private val jFCPlibPluginConnection = jFCPlibPluginConnection(fcpConnection, "test.TargetPlugin")
+	private val jFCPlibPluginConnection = createConnectionRequestCountingPluginConnection(fcpConnection)
 
 }
